bde83b467ce6f23af10808fd7b131206908125e9
oriongin <97828002+oriongin@users.noreply.github.com>
2026-04-24 15:56:30 +0700

    rm

--- hummingbot/core/rate_oracle/rate_oracle.py
+++ hummingbot/core/rate_oracle/rate_oracle.py
@@ -7,12 +7,10 @@ import hummingbot.client.settings  # noqa
 from hummingbot.connector.utils import combine_to_hb_trading_pair
 from hummingbot.core.network_base import NetworkBase
 from hummingbot.core.network_iterator import NetworkStatus
-from hummingbot.core.rate_oracle.sources.ascend_ex_rate_source import AscendExRateSource
 from hummingbot.core.rate_oracle.sources.binance_rate_source import BinanceRateSource
 from hummingbot.core.rate_oracle.sources.coin_cap_rate_source import CoinCapRateSource
 from hummingbot.core.rate_oracle.sources.coin_gecko_rate_source import CoinGeckoRateSource
 from hummingbot.core.rate_oracle.sources.gate_io_rate_source import GateIoRateSource
-from hummingbot.core.rate_oracle.sources.kucoin_rate_source import KucoinRateSource
 from hummingbot.core.rate_oracle.sources.rate_source_base import RateSourceBase
 from hummingbot.core.rate_oracle.utils import find_rate
 from hummingbot.core.utils.async_utils import safe_ensure_future
@@ -22,8 +20,6 @@ RATE_ORACLE_SOURCES = {
     "binance": BinanceRateSource,
     "coin_gecko": CoinGeckoRateSource,
     "coin_cap": CoinCapRateSource,
-    "kucoin": KucoinRateSource,
-    "ascend_ex": AscendExRateSource,
     "gate_io": GateIoRateSource,
 }
 
